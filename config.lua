4738230afc3077aa74d79382b557bc3a5c36d5e8
Paulo Koch <paulo.koch@g.globo>
2023-01-09 15:02:16 -0300

    fix

--- config.lua
+++ config.lua
@@ -29,7 +29,7 @@ lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 -- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
 -- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
 local _, actions = pcall(require, "telescope.actions")
- lvim.builtin.telescope.defaults.mappings = {
+lvim.builtin.telescope.defaults.mappings = {
    -- for input mode
    i = {
      ["<C-j>"] = actions.move_selection_next,
